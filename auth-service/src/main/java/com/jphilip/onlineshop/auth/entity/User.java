840cf76a00ed3af31c337c0b851c64ce53e19566
jphilip-dev <john.p.s.de.roxas+git@gmail.com>
2025-05-24 02:32:28 +0800

    Add Builder annotation

--- auth-service/src/main/java/com/jphilip/onlineshop/auth/entity/User.java
+++ auth-service/src/main/java/com/jphilip/onlineshop/auth/entity/User.java
@@ -2,6 +2,7 @@ package com.jphilip.onlineshop.auth.entity;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -12,6 +13,7 @@ import java.util.Set;
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+@Builder
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
